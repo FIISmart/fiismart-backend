223fcc27dd61e9876faf8a30f0c4f1fc41fc9b19
Cristian Gavrilean <cristiangavrilean@gmail.com>
2026-03-27 18:37:32 +0200

    Add global exception handling (404, 400, validation errors)

---
+++ src/main/java/com/fiismart/backend/course/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.fiismart.backend.course.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/fiismart/backend/course/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.fiismart.backend.course.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
