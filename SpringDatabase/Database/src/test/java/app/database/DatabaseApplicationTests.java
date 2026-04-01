3493eb79dd792a2a91387ddcaa6704ee8fff6e44
CovitaSebastianMarian <sebastianmarian0000@gmail.com>
2026-04-01 09:58:54 +0300

    small fix

--- SpringDatabase/Database/src/test/java/app/database/DatabaseApplicationTests.java
+++ SpringDatabase/Database/src/test/java/app/database/DatabaseApplicationTests.java
@@ -1,13 +1,12 @@
 package app.database;
 
+import com.app.FiiSmartApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FiiSmartApplication.class)
 class DatabaseApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
