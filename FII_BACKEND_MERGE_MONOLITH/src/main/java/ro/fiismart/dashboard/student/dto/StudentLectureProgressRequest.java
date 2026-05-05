d8116ff3e26154eebfc685256ccea6b5c92713b4
Rubert183 <113166398+Rubert183@users.noreply.github.com>
2026-05-05 19:38:19 +0300

    Revert "backend integration"

--- FII_BACKEND_MERGE_MONOLITH/src/main/java/ro/fiismart/dashboard/student/dto/StudentLectureProgressRequest.java
+++
@@ -1,9 +0,0 @@
-package ro.fiismart.dashboard.student.dto;
-
-import lombok.Data;
-
-@Data
-public class StudentLectureProgressRequest {
-    private int watchedPercent;
-    private int positionSecs;
-}
